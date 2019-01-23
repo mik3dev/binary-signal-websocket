d3ec620314e29a47ec4649fbb5f05be1613b8490
Mik3Dev <miguel.acosta1978@gmail.com>
2019-01-23 11:12:50 -0300

    .

--- index.js
+++ index.js
@@ -43,12 +43,22 @@ setInterval( () => {
     })
     Promise.all(promises).then(r => {
         const result = config.Instruments.map(item => {
-            const data = r.filter(subItem => subItem.instrument == item)
+            const data = r.filter(subItem => subItem.instrument == item);
+            const high = data.filter(subItem => subItem.flag == 'High')[0];
+            const mid = data.filter(subItem => subItem.flag == 'Mid')[0];
+            const low = data.filter(subItem => subItem.flag == 'Low')[0];
+            const alertType = high.signal;
+            let actionType = 'NEUTRAL';
+            if(high.signal == 'BUY' && mid.signal == 'BUY' && low.signal == "BUY") actionType = 'BUY';
+            else if(high.signal == 'SELL' && mid.signal == 'SELL' && low.signal == "SELL") actionType = 'SELL';
+
             return {
                 instrument: item,
-                high: data.filter(subItem => subItem.flag == 'High')[0],
-                mid: data.filter(subItem => subItem.flag == 'Mid')[0],
-                low: data.filter(subItem => subItem.flag == 'Low')[0],
+                high,
+                mid,
+                low,
+                alertType,
+                actionType
             }
         });
         io.emit('fxData', result);
